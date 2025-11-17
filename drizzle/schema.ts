ebfbabbe43a71fca088ffa9484f719f92e804e1a
Antonio Carlos <156712723+AntonioC0@users.noreply.github.com>
2025-11-17 19:49:36 -0300

    Change import from mysql-core to pg-core

--- drizzle/schema.ts
+++ drizzle/schema.ts
@@ -1,4 +1,4 @@
-import { int, mysqlEnum, mysqlTable, text, timestamp, varchar, decimal } from "drizzle-orm/mysql-core";
+import { int, mysqlEnum, mysqlTable, text, timestamp, varchar, decimal } from "drizzle-orm/pg-core";
 
 /**
  * Core user table backing auth flow.
